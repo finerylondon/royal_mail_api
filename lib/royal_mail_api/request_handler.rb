a67aa83c8b9c1ba5a7e7b2748a1a8139103cf8d2
Neil Turner <neil@neilturner.me>
2016-10-10 16:06:47 +0100

    Fix hash error

--- lib/royal_mail_api/request_handler.rb
+++ lib/royal_mail_api/request_handler.rb
@@ -37,8 +37,8 @@ module RoyalMailApi
         log: config.logger.level.zero?,
         pretty_print_xml: true,
         headers: {
-          'X-IBM-Client-Id': config.application_id
-          'X-IBM-Client-Secret': config.application_secret
+          :'X-IBM-Client-Id' => config.application_id
+          :'X-IBM-Client-Secret' => config.application_secret
         }
       )
     end
